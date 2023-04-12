76ee476bfaf5ea7e839edffad7dbcdb5ca56d54f
dlsirllbs <wl17857115263@outlook.com>
2023-04-13 00:36:02 +0800

    finish lab06

--- lab/lab06/lab06.py
+++ lab/lab06/lab06.py
@@ -17,6 +17,11 @@ def make_adder_inc(a):
     11
     """
     "*** YOUR CODE HERE ***"
+    def adder(b):
+        adder.counter += 1
+        return a + b + adder.counter
+    adder.counter = -1
+    return adder
 
 
 def make_fib():
@@ -43,7 +48,20 @@ def make_fib():
     True
     """
     "*** YOUR CODE HERE ***"
-
+    def fib():
+        sum = 0
+        if fib.counter == 0 or fib.counter == 1:
+            sum = fib.counter
+        else:
+            a,b = 0, 1
+            for _ in range(fib.counter - 1):
+                sum = a + b
+                a = b
+                b = sum
+        fib.counter += 1
+        return sum
+    fib.counter = 0
+    return fib
 
 def insert_items(lst, entry, elem):
     """
@@ -62,4 +80,14 @@ def insert_items(lst, entry, elem):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    lst1 = []
+    for item in lst[:]:
+        lst1.append(item)
+        if item == entry:
+            lst1.append(elem)
+    for i in range(len(lst1)):
+        if i < len(lst):
+            lst[i] = lst1[i]
+        else:
+            lst.append(lst1[i])
+    return lst
